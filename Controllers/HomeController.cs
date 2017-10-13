99544fc86d9b9460ab0d389259f1cb62ab0f8384
Marco Mendes <marco.mendes@arkhi.com.br>
2017-10-13 09:06:43 -0300

    Projeto simples para demonstrar o uso de autenticacao e autorização em ASP.NET Core 2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnet_seguro.Models;
+using Microsoft.AspNetCore.Identity;
+using aspnet_seguro.Services;
+using Microsoft.Extensions.Logging;
+
+namespace aspnet_seguro.Controllers
+{
+    public class HomeController : Controller
+    {
+         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger _logger;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+
+            var user = _userManager.GetUserAsync(HttpContext.User);
+            Task<IList<String>> resultados = _userManager.GetRolesAsync(user.Result);
+
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/SeguroController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using aspnet_seguro.Models;
+using aspnet_seguro.Models.AccountViewModels;
+using aspnet_seguro.Services;
+
+namespace aspnet_seguro.Controllers
+{
+    [Authorize(Roles="admin")]
+    [Route("api/[controller]")]
+    /*
+    Este é um controlador com acesso seguro. 
+    Apenas usuarios com o papel de administador podem acessar este recurso.
+    Para que este codigo funcione eh necessario que o banco de dados tenha:
+     - O papel admin cadastrado na tabela AspNetRoles
+     - O relacionamento entre o usuario e o papel cadastrado na tabela AspNetUserRoles
+     - Neste exemplo o usuario chamado usuario1@empresa.com.br esta vinculado ao papel admin.
+     - Já o usuario chamado usuario2@empresa.com.br nao tem esta vinculacao.
+     - As senhas destes usuarios para teste são respectivamenteß Usuario1! e Usuario2!
+     */
+    public class SeguroController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Ei. Este é um acesso apenas para administradores");
+        }
+    }
+
+}
